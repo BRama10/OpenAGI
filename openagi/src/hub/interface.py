4998756de5d86fde9ddcc16b8fb381cd7ebfd4e0
BRama10 <grand.hunter.dark.15@gmail.com>
2024-06-05 21:52:14 -0400

    f

---
+++ openagi/src/hub/interface.py
@@ -0,0 +1,10 @@
+
+
+class AgentConfig:
+    def __init__(self, tool_config: str, agent_instructions: str):
+        self.tools = tool_config
+        self.agent = agent_instructions
+
+    def 
+
+def 
